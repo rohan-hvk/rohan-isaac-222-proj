9bf23d2729c238fecdccb7f920b37c7d750da1f5
Rohan Kanumuri <rohan.kanumuri@peopletech.com>
2025-04-30 11:33:27 -0700

    added more eda stuff, fixed hypothesis test

--- utils.py
+++ utils.py
@@ -1,17 +1,9 @@
 import pandas as pd
 
 def clean_enriched_netflix_data(df):
-    # Date is datetime
-    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%y', errors='coerce')
-    
-    # imssing stuff is n/a
+    # parse any style automatically:
+    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+
     df['Series Name'] = df['Series Name'].fillna('N/A')
     df['Season and Episode'] = df['Season and Episode'].fillna('N/A')
-    
-    # Reorder columns
-    df = df[['Title', 'Date', 'Type', 'Series Name', 'Season and Episode', 'Watch Year']]
-    
-    return df
-
-
-
+    return df[['Title','Date','Type','Series Name','Season and Episode','Watch Year']]
